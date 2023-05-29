1178e73fc62690db82ea98cedf30fdeb4bd57027
Rizqi Nur Andi Putra <92218072+Thecaracter@users.noreply.github.com>
2023-05-30 02:54:34 +0700

    .

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -24,7 +24,7 @@ class PaymentController extends Controller
 
         $params = [
             'transaction_details' => [
-                'order_id' => 'ORDER-' . $pendaftaran->id,
+                'order_id' => 'Pembayaran-' . $pendaftaran->id,
                 'gross_amount' => (float) $pendaftaran->lomba->harga,
                 // Mengambil harga dari relasi 'lomba'
             ],
